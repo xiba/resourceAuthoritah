3ba1e9283db6b0e4ecc58c4a11c604d3e5116c1b
Saif M Helmy <saif.m.helmy@gmail.com>
2016-10-08 20:33:55 +0200

    Added lock release support, unit tests.
    Also added an upper limit to expiration durations.
    Also added validation of content-type headers (only application/json is accepted)

--- flaskApp.py
+++ flaskApp.py
@@ -8,32 +8,55 @@ app = Flask(__name__)
 
 @app.route('/resources/<fResourceName>', methods=['GET'])
 def queryResource(fResourceName):
-    replyDict = {}
+    if(request.content_type != 'application/json'):
+        return "", 400
+
+    replyDict = {'resource' : fResourceName}
     if(resourceInterface.isResourceLocked(fResourceName)):
-        replyDict = {fResourceName : 'locked'}
+        replyDict[fResourceName] = 'locked'
     else:
-        replyDict = {fResourceName : 'free'}
-    return jsonify(replyDict)
+        replyDict[fResourceName] = 'free'
+    return jsonify(**replyDict)
 
 @app.route('/resources/<fResourceName>', methods=['POST'])
 def acquireResource(fResourceName):
-    replyDict = {}
+    if(request.content_type != 'application/json'):
+        return "", 400
+
+    replyDict = {'resource' : fResourceName}
     requestData = request.get_json()
 
     if('expiry' not in requestData):
         requestData['expiry'] = 5
+    elif(requestData['expiry'] > 120): #whatever limit, should be configured according to the usecase of the server
+        replyDict['error'] = 'Expiration value of ' + str(requestData['expiry']) + ' is too large'
+        return jsonify(**replyDict)
     if('id' not in requestData):
-        replyDict = {'Argument error' : 'ID not provided in request'}
-        return jsonify(replyDict)
+        replyDict['error'] = 'ID not provided in request'
+        return jsonify(**replyDict)
 
     expiryVal = requestData['expiry']
     idVal = requestData['id']
 
-    if(resourceInterface.acquireResource(fResourceName, idVal, expiryVal)):
-        replyDict = {fResourceName : 'acquired'}
-    else:
-        replyDict = {fResourceName : 'rejected'}
-    return jsonify(replyDict)
+    if(not resourceInterface.acquireResource(fResourceName, idVal, expiryVal)):
+        replyDict['error'] = 'resource is already locked'
+    return jsonify(**replyDict)
+
+@app.route('/resources/<fResourceName>', methods=['DELETE'])
+def releaseResource(fResourceName):
+    if(request.content_type != 'application/json'):
+        return "", 400
+
+    replyDict = {'resource' : fResourceName}
+    requestData = request.get_json()
+    if('id' not in requestData):
+        replyDict['error'] = 'ID not provided in request'
+        return jsonify(**replyDict)
+
+    isDeleted = resourceInterface.releaseResource(fResourceName, requestData['id'])
+    if(not isDeleted):
+        replyDict['error'] = 'Wrong id'
+    return jsonify(**replyDict)
 
 if __name__ == '__main__':
     app.run()
--- resourceInterface.py
+++ resourceInterface.py
@@ -1,15 +1,25 @@
 import redis
 
-class ResourceDoesNotExist(ValueError):
-    pass
-
 def isResourceLocked(fResourceName):
     '''Returns true if said resource is locked. Note that we don't check if the resource exists.'''
-    r = redis.Redis(host='127.0.0.1', port=6379, password=None)
+    r = redis.Redis() #TODO: get server config from the flask app
     return True if r.get(fResourceName) else False
 
 def acquireResource(fResourceName, fValue, fExpiry):
     '''Attempts to acqurie the lock. Returns None on failure'''
-    r = redis.Redis(host='127.0.0.1', port=6379, password=None)
+    r = redis.Redis() #TODO: get server config from the flask app
     setResult = r.set(name=fResourceName, value=fValue, nx=True, ex=fExpiry)
     return True if setResult else False
+
+def releaseResource(fResourceName, fValue):
+    '''Attempts to release the lock, the passed value must match it's value.'''
+    r = redis.Redis() #TODO: get server config from the flask app
+    storedValue = r.get(fResourceName)
+    if(storedValue == None):
+        return True
+
+    if(storedValue.decode('utf-8') != fValue):
+        return False
+
+    r.delete(fResourceName)
+    return True
